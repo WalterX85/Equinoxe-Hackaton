e05ce77600764931f12d633b6f12f06daf2ebf4b
AllanCaplain <79872711+AllanCaplain@users.noreply.github.com>
2021-05-11 19:56:15 +0200

    fix conflicts

--- equinoxe/src/component/Header.jsx
+++ equinoxe/src/component/Header.jsx
@@ -4,18 +4,6 @@ import cssHeader from './css/cssHeader.css';
 
 const Header = () => (
     <header className="header">
-      <div className="welcome">
-        <h1>WELCOME TO EQUINOXE</h1>
-      </div>
-      <div className="thefourth">
-        <nav className="navlist">
-  
-          <NavLink className="main-link" activeClassName="active" exact to="/">
-            Home
-          </NavLink>
-  
-          <NavLink className="main-link" activeClassName="active" exact to="/seasons">
-
         <div className="headercomp">
         <nav className="navlist">
           <NavLink className="link" activeClassName="active" exact to="/">
@@ -23,11 +11,8 @@ const Header = () => (
           </NavLink>
           <h1>EQUINOXE</h1>
           <NavLink className="link" activeClassName="active" exact to="/seasons">
-
             Seasons
           </NavLink>
-
-          
         </nav>
         </div>
     </header>
