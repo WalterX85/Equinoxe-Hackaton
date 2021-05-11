74248ba7ed4f514dc10d23296a3a2c811527c2b8
AllanCaplain <79872711+AllanCaplain@users.noreply.github.com>
2021-05-11 20:52:05 +0200

    changes on some files

--- equinoxe/src/component/Seasons.jsx
+++ equinoxe/src/component/Seasons.jsx
@@ -6,40 +6,39 @@ function Seasons() {
 	<div className="linkcontainer">
 		<nav className="navlinks">
 			<NavLink className="link janvier" activeClassName="active" exact to="/seasons/janvier">
-				Janvier
 			</NavLink>
+
 			<NavLink className="link fevrier" activeClassName="active" exact to="/seasons/fevrier">
-				Février
 			</NavLink>
+
 			<NavLink className="link mars" activeClassName="active" exact to="/seasons/mars">
-				Mars
 			</NavLink>
+
 			<NavLink className="link avril" activeClassName="active" exact to="/seasons/avril">
-				Avril
 			</NavLink>
+
 			<NavLink className="link mai" activeClassName="active" exact to="/seasons/mai">
-				Mai
 			</NavLink>
+
 			<NavLink className="link juin" activeClassName="active" exact to="/seasons/juin">
-				Juin
 			</NavLink>
+
 			<NavLink className="link juillet" activeClassName="active" exact to="/seasons/juillet">
-				Juillet
 			</NavLink>
+
 			<NavLink className="link aout" activeClassName="active" exact to="/seasons/aout">
-				Aout
 			</NavLink>
+
 			<NavLink className="link septembre" activeClassName="active" exact to="/seasons/septembre">
-				Septembre
 			</NavLink>
+
 			<NavLink className="link octobre" activeClassName="active" exact to="/seasons/octobre">
-				Octobre
 			</NavLink>
+
 			<NavLink className="link novembre" activeClassName="active" exact to="/seasons/novembre">
-				Novembre
 			</NavLink>
+			
 			<NavLink className="link decembre" activeClassName="active" exact to="/seasons/decembre">
-				Décembre
 			</NavLink>
 		</nav>
 
