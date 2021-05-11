0371cfd968752b567e3cb0dfb6a29d021454c129
AllanCaplain <79872711+AllanCaplain@users.noreply.github.com>
2021-05-11 15:47:41 +0200

    Header style done

--- equinoxe/src/component/Header.jsx
+++ equinoxe/src/component/Header.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import cssHeader from './css/cssHeader.css';
 
 const Header = () => (
     <header className="header">
-      <div className="gridstyle">
-        <h1>WELCOME TO EQUINOXE</h1>
-      </div>
-      <div className="thefourth">
+        <div className="headercomp">
         <nav className="navlist">
-  
           <NavLink className="link" activeClassName="active" exact to="/">
             Home
           </NavLink>
-  
+          <h1>EQUINOXE</h1>
           <NavLink className="link" activeClassName="active" exact to="/seasons">
             Seasons
           </NavLink>
         </nav>
-      </div>
+        </div>
     </header>
   );
 
